b637d121e0bf89bfd142c19593a88689b08e4482
Fulwin <vinty_email@163.com>
2017-09-28 18:05:14 +0800

    ecn详情页增加耗时计算

--- Atop/Home/Common/function.php
+++ Atop/Home/Common/function.php
@@ -938,3 +938,37 @@ function traverse_document(&$arr){
 function getCurrentURL(){
     return $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 }
+
+/**
+ * 时间差计算
+ * @param Timestamp $time
+ * @return String Time Elapsed
+ * @author Shelley Shyan
+ * @copyright http://phparch.cn (Professional PHP Architecture)
+ */
+function time2Units ($time){
+    $year   = floor($time / 60 / 60 / 24 / 365);
+    $time  -= $year * 60 * 60 * 24 * 365;
+    $month  = floor($time / 60 / 60 / 24 / 30);
+    $time  -= $month * 60 * 60 * 24 * 30;
+    $week   = floor($time / 60 / 60 / 24 / 7);
+    $time  -= $week * 60 * 60 * 24 * 7;
+    $day    = floor($time / 60 / 60 / 24);
+    $time  -= $day * 60 * 60 * 24;
+    $hour   = floor($time / 60 / 60);
+    $time  -= $hour * 60 * 60;
+    $minute = floor($time / 60);
+    $time  -= $minute * 60;
+    $second = $time;
+    $elapse = '';
+    $unitArr = array('年'  =>'year', '个月'=>'month',  '周'=>'week', '天'=>'day',
+        '小时'=>'hour', '分钟'=>'minute', '秒'=>'second'
+    );
+    foreach ( $unitArr as $cn => $u ){
+        if ( $$u > 0 ){
+            $elapse = $$u . $cn;
+            break;
+        }
+    }
+    return $elapse;
+}
