2b8efe455fdb6f97c9d31ca3fe4a0c6c8f0990fb
Fulwin <vinty_email@163.com>
2017-11-14 16:16:16 +0800

    新增公告栏功能

---
+++ Public/home/js/bulletin/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by GCX on 2017/10/30.
+ */
+$(function(){
+
+    layui.use(['form','layer'],function(){
+        var layer = layui.layer,
+            form = layui.form();
+
+
+        
+    });
+
+});
